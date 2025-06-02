f1a24efcddda6ba5ac93e31070255c862b269550
V-inTim <vinichuk.timofey@mail.ru>
2025-06-02 23:18:56 +0300

    Create structure.

---
+++ src/main/java/com/example/shift_control_fingerprints/ShiftControlFingerprintsApplication.java
@@ -0,0 +1,13 @@
+package com.example.shift_control_fingerprints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftControlFingerprintsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftControlFingerprintsApplication.class, args);
+	}
+
+}
