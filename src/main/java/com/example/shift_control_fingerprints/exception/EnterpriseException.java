d0f4778dec55d599fe6737525e08ed35a83c6e6d
V-inTim <vinichuk.timofey@mail.ru>
2025-06-05 14:42:34 +0300

    Add EnterpriseException.

---
+++ src/main/java/com/example/shift_control_fingerprints/exception/EnterpriseException.java
@@ -0,0 +1,7 @@
+package com.example.shift_control_fingerprints.exception;
+
+public class EnterpriseException extends RuntimeException{
+    public EnterpriseException(String message) {
+        super(message);
+    }
+}
